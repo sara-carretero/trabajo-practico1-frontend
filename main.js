e0b94f42ed4139fa2374133f437ae633cd508d5e
saracarretero <carreteroprogramar@gmail.com>
2025-08-17 00:58:12 -0300

    Creo la función sendMessage()

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ const chatMessages = document.querySelector(".chat-messages")
 //Invocación de función. Cuando el botón reciba un "click", ejecutará la función sendMessage.
 sendButton.addEventListener("click", sendMessage)
 
+// declaración de función. Una función tiene 4 partes fundamentales:
+// 1. palabra reservada (son palabras que ya existen en js y que tienen un uso). -> function
+//2. nombre de la función (siempre debe ser un verbo) -> sendMessage
+//3. parentesis (son los parámentros, es decir herramientas internas para la función) -> (sendButton)
+//4. llaves (tecnicamente se llama 'escope'. Es la zona privada de la función) -> {}
+
+function sendMessage() {
+
+}
