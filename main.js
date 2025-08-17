71ba952e3e66c49ee2a23dc7eb2edabdaa9d2ced
saracarretero <carreteroprogramar@gmail.com>
2025-08-17 00:57:33 -0300

    Preparo al botón para que esté atento al click y ejecute una función

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const inputMessage = document.querySelector(".text")
 const sendButton = document.querySelector(".buttonSend")
 const chatMessages = document.querySelector(".chat-messages")
 
+//Invocación de función. Cuando el botón reciba un "click", ejecutará la función sendMessage.
+sendButton.addEventListener("click", sendMessage)
+
