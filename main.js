54bbb8fa9bc906ad918158a68c8f5acf356cc01e
saracarretero <carreteroprogramar@gmail.com>
2025-08-18 19:39:17 -0300

    Algoritmo para enviar msjs y recibir uno de vuelta.

--- main.js
+++ main.js
@@ -1,30 +1,56 @@
-const inputMessage = document.querySelector(".text")
-const sendButton = document.querySelector(".buttonSend")
-const chatMessages = document.querySelector(".chat-messages")
+//Se le agrega el signo $ adelante de las variables que tienen almacenaado un elemento HTML para una ayuda visual.
+const $inputMessage = document.querySelector(".text")
+const $sendButton = document.querySelector("#buttonSend")
+const $chatMessages = document.querySelector(".chat-messages")
 
 //Invocación de función. Cuando el botón reciba un "click", ejecutará la función sendMessage.
-sendButton.addEventListener("click", sendMessage)
+$sendButton.addEventListener("click", sendMessage)
 
 // declaración de función. Una función tiene 4 partes fundamentales:
 // 1. palabra reservada (son palabras que ya existen en js y que tienen un uso). -> function
 //2. nombre de la función (siempre debe ser un verbo) -> sendMessage
 //3. parentesis (son los parámentros, es decir herramientas internas para la función) -> (sendButton)
-//4. llaves (tecnicamente se llama 'escope'. Es la zona privada de la función) -> {}
+//4. llaves (tecnicamente se llama 'scope'. Es la zona privada de la función) -> {}
 
 function sendMessage() {
-  const message = inputMessage.value
+  const message = $inputMessage.value
 
   //Creo un div para agregar el mensaje.
-  const divMessage = document.createElement("div")
+  const $divMessage = document.createElement("div")
   //Accedo a la lista de clases de divMessage, y le agrego una clase con add.
-  divMessage.classList.add("message", "sent")
+  $divMessage.classList.add("message", "sent")
 
-  const text = document.createElement("p")
-  text.textContent = message
+  const $text = document.createElement("p")
+  $text.textContent = message
 
-  divMessage.appendChild(text)
+  $divMessage.appendChild($text)
+
+  const $time = document.createElement("p")
+  $time.classList.add("timestamp")
+  //Coloco la hora de cuando se envió el msj.
+  $time.textContent = new Date().toLocaleTimeString()
+
+  $divMessage.appendChild(time)
 
   //Le agrego un hijo de la sección, al chatMessages, el hijo es containerMessage.
-  chatMessages.appendChild(divMessage)
+  $chatMessages.appendChild($divMessage)
 
+  //Limpiamos el input con un "" que llama string vacío.
+  $inputMessage.value = ""
+
+  setTimeout(function () {
+    const $divMessage = document.createElement("div")
+    $divMessage.classList.add("message", "received")
+
+    const $response = document.createElement("p")
+    $response.textContent = `Recibido ${message} ✅`
+
+    const $time = document.createElement("p")
+    $time.classList.add("timestamp")
+    $time.textContent = new Date().toLocaleTimeString()
+
+    $divMessage.appendChild(response)
+    $divMessage.appendChild(time)
+    $chatMessages.appendChild($divMessage)
+  }, 5000)
 }
