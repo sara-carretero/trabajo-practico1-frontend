dd0e27f11d8a740f4d4e6f9c9bfb9981ef12adbb
saracarretero <carreteroprogramar@gmail.com>
2025-08-24 21:04:47 -0300

    Creacion ejercicios html, css, js

---
+++ ejercicios-html-css-js/main.js
@@ -0,0 +1,12 @@
+//Ejemplo 8 – Array + forEach + Condicional
+
+let numbers = [2, 4, 6, 8, 10];
+numbers.forEach(num => {
+  if (num % 2 = 0) {
+  console.log(num + " es par");
+} else {
+  console.log(num + " es impar");
+}
+});
+
+//----------------------------------------------------------
